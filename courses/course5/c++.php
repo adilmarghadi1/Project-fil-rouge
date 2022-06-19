f2a5d831f03464a504e34eb92c106933486cda4c
adilmarghadi1 <adilmarghadi2020@gmail.com>
2022-06-20 00:32:55 +0100

    Add

---
+++ courses/course5/c++.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Design Html & Css</title>
+    <link rel="stylesheet" href="../../style.css">
+</head>
+<body>
+    <style>
+        
+.video {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top:50px;
+}
+.title_1 {
+    text-align: center;
+    margin-top: 90px;
+    font-size: 1.9em;
+    color: #14213d;
+}
+
+</style>
+<div class="nav1">
+            <div class="left">
+                <ul class="nav-menu1">
+                    <li class="nav-item1">
+                        <i class="fas fa-phone-alt"> </i>
+                        <a href="tel:0708700319">0708700319</a>
+                    </li>
+
+                    <li class="nav-item1">
+                        <i class="far fa-envelope"> </i>
+                        <a href="#"> Contact.solicode@gmail.com</a>
+                    </li>
+                </ul>
+            </div>
+            <ul class="nav-menu1">
+                <div class="center">
+                    <li class="nav-item1">
+                        <i class="icon fas fa-user"></i>
+                        <a href="login.html">Sign In</a>
+                    </li>
+
+                    <li class="nav-item1">
+                        <i class="fas fa-sign-in-alt"></i>
+                        <a href="register.html">Register</a>
+                    </li>
+                </div>
+                <div class="vl"></div>
+                    <div class="right">
+                    <li class="nav-item1">
+                        <a href="https://twitter.com/" target="_blanck">
+                    <i class="fab fa-twitter"></i></a>
+                    </li>
+
+                    <li class="nav-item1">
+                        <a href="https://instagram.com/" target="_blanck">
+                    <i class="fab fa-instagram"></i></a>
+                    </li>
+
+                    <li class="nav-item1">
+                        <a href="https://linkedin.com/" target="_blanck">
+                    <i class="fab fa-linkedin"></i></a>
+                    </li>
+
+                    <li class="nav-item1">
+                        <a href="https://github.com/" target="_blanck">
+                            <i class="fab fa-github"></i></a>
+                    </li>
+                    
+                </div>
+                </ul>
+            </div>
+            <header class="header">
+           
+                   
+               
+           <nav class="navbar">
+               <div class="logo">
+                   <a href="../../index.html">Solicode<span class="design"> Online</span></a>
+               </div>  
+               <ul class="nav-menu">
+                 
+                   <li class="nav-item">
+                       <a href="../../index.html" class="nav-link">Home</a>
+                   </li>
+   
+                   <li class="nav-item">
+                       <a href="../../courses.html" class="nav-link">Courses</a>
+                   </li>
+   
+   
+                   <li class="nav-item">
+                       <a href="../../teachers.html" class="nav-link">Techears</a>
+                   </li>
+   
+   
+   
+                   <li class="nav-item">
+                       <a href="../../blog.html" class="nav-link">Blog</a>
+                   </li>
+   
+                   <li class="nav-item">
+                       <a href="../../about.html" class="nav-link">About</a>
+                   </li>
+   
+                  
+               </ul>
+               <div class="hamburger">
+                   <span class="bar"></span>
+                   <span class="bar"></span>
+                   <span class="bar"></span>
+               </div> 
+               
+           </nav> 
+       </header>
+
+
+       <section>
+            <h1 class='title_1'> Learn C++ Course</h1>
+
+            <div class='video'>
+            <iframe width="70%" height="610" src="https://www.youtube-nocookie.com/embed/96hZpsP0bbE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>       </section>
+</body>
+</html>
